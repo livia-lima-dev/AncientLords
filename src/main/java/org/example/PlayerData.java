183c8adf24dc53923ce8a2c5289747dcbbd89bc6
Lara <laracastro@edu.unifor.br>
2026-05-27 16:49:25 -0300

    criação da classe playerData para armazenar as informações do jogador e criação de 'contador' nas tels de derrota e vitoria

---
+++ src/main/java/org/example/PlayerData.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class PlayerData {
+
+    public static String nome = "Jogador";
+
+    public static int nivel = 15;
+
+    public static int totalPartidas = 52;
+
+    public static int vitorias = 34;
+
+    public static int derrotas = 18;
+
+    public static String classeMaisJogada = "Mago";
+}
